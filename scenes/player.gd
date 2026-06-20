0d82406b2510aacc36ae52794a44e955a7039af7
avycado13 <108358183+avycado13@users.noreply.github.com>
2026-06-20 02:17:39 -0700

    git plugin

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,28 +1,48 @@
 extends CharacterBody2D
 
-
-
-
 const JUMP_VELOCITY = -350
 
 @onready var sprite: AnimatedSprite2D = $sprite
+@onready var mine_ray: RayCast2D = $RayCast2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
-		velocity.y +=600 * delta
+		velocity.y += 600 * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Jump
+	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if Input.is_action_pressed("ui_left"):
+	# Movement
+	if Input.is_action_pressed("left"):
 		velocity.x = -120
 		sprite.flip_h = false
-	elif Input.is_action_pressed("ui_right"):
+		mine_ray.target_position.x = -16
+
+	elif Input.is_action_pressed("right"):
 		velocity.x = 120
 		sprite.flip_h = true
+		mine_ray.target_position.x = 16
+
+	else:
+		velocity.x = 0
+
+	# Mining
+	#if Input.is_action_just_pressed("mine"):
+		#mine()
 
 	move_and_slide()
+
+
+func mine():
+	if mine_ray.is_colliding():
+		var pos = mine_ray.get_collision_point()
+
+		var tilemap = get_parent().get_node("TileMap")
+
+		var cell = tilemap.local_to_map(
+			tilemap.to_local(pos)
+		)
+
+		tilemap.erase_cell(0, cell)
