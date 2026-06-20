82a3a1d284e43eaff94ef58be90d11e1847cda9e
Coding-Koala222 <168612350+Coding-Koala222@users.noreply.github.com>
2026-06-20 16:31:15 -0700

    Delete scenes/play.gd and remove weird merge symbols

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,10 @@
 extends CharacterBody2D
 
-<<<<<<< HEAD
-
 
 
 const JUMP_VELOCITY = -325 #Defines constants
-=======
-const JUMP_VELOCITY = -325
 const WALL_JUMP_X = 180
->>>>>>> 5ff5c2a2eec06f2eaeed73ddb160cb534c968c86
+
 
 var dir := -1 #Var for the players direction
 
@@ -16,7 +12,7 @@ var dir := -1 #Var for the players direction
 @onready var tile_map_layer: TileMapLayer = $"../TileMapLayer" #Loads two other noads, the players sprite and the tilemap.
 
 func _physics_process(delta: float) -> void:
-<<<<<<< HEAD
+
 	# Add the gravity.
 	if not is_on_floor(): #Manages falling
 		velocity.y +=600 * delta
@@ -26,8 +22,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	
-	if Input.is_action_pressed("ui_left"): #Left and right movement
-=======
 	# Gravity
 	if not is_on_floor():
 		velocity.y += 600 * delta
@@ -50,21 +44,18 @@ func _physics_process(delta: float) -> void:
 				velocity.x = -WALL_JUMP_X
 
 	# Horizontal movement
-	if Input.is_action_pressed("ui_left"):
->>>>>>> 5ff5c2a2eec06f2eaeed73ddb160cb534c968c86
+	if Input.is_action_pressed("left"):
 		velocity.x = -120
 		sprite.flip_h = false
 		dir = -1
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("right"):
 		velocity.x = 120
 		sprite.flip_h = true
 		dir = 1
 	else:
-<<<<<<< HEAD
 		velocity.x = 0
 		
 	if Input.is_action_just_pressed("ui_accept"): # This checks if the mine button is pressed and if so mines
-=======
 		# Don't stop horizontal movement immediately while wall jumping
 		if is_on_floor():
 			velocity.x = 0
@@ -73,7 +64,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = 50
 	# Mining
 	if Input.is_action_just_pressed("ui_accept"):
->>>>>>> 5ff5c2a2eec06f2eaeed73ddb160cb534c968c86
 		if Input.is_action_pressed("ui_down"):
 			tile_map_layer.destroy_tile(position + Vector2(0, 16))
 			velocity.y = 70
