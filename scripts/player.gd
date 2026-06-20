b823bb483fb1b593082ec11138e8698db1e202df
Omelette <desh.kian@gmail.com>
2026-06-20 16:27:50 -0700

    new cheese iirc

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -325
+const JUMP_VELOCITY = -250
 const WALL_JUMP_X = 180
 
 var dir := -1 #Var for the players direction
@@ -9,27 +9,29 @@ var cheese := 0
 @onready var left: RayCast2D = $Left
 @onready var sprite: AnimatedSprite2D = $sprite
 @onready var tile_map_layer: TileMapLayer = $"../TileMapLayer" #Loads two other noads, the players sprite and the tilemap.
+@onready var rich_text_label: RichTextLabel = $"../CanvasLayer/RichTextLabel"
+
 
 func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
-		velocity.y += 600 * delta
+		velocity.y += 400 * delta
 
 	# Normal jump
 	if Input.is_action_just_pressed("up"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
-
+		
 		
 		elif left.is_colliding():
 			# Wall is on left
-			velocity.y = -250
+			velocity.y = -175
 			velocity.x = WALL_JUMP_X
 		elif right.is_colliding():
 			# Wall is on right
-			velocity.y = -250
+			velocity.y = -175
 			velocity.x = -WALL_JUMP_X
-
+	
 	# Horizontal movement
 	if Input.is_action_pressed("left"):
 		velocity.x = -120
@@ -41,8 +43,8 @@ func _physics_process(delta: float) -> void:
 		dir = 1
 	else:
 		# Don't stop horizontal movement immediately while wall jumping
-		if is_on_floor():
-			velocity.x = 0
+		
+		velocity.x = 0
 
 	
 	# Mining
@@ -54,5 +56,5 @@ func _physics_process(delta: float) -> void:
 			tile_map_layer.destroy_tile(position - Vector2(0, 16))
 		else:
 			tile_map_layer.destroy_tile(position + dir * Vector2(16, 0))
-
+		rich_text_label.text = str(cheese)
 	move_and_slide()
