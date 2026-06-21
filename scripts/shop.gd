272b35abecb2edd0a4272aab31ae3a72b47deaf8
Cubeshifter <desh.kian@gmail.com>
2026-06-21 13:43:07 -0700

    fixed shop now it works yeah

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -14,15 +14,43 @@ func _process(delta: float) -> void:
 	for i in range(5):
 		
 		get_node("HBoxContainer/RichTextLabel"+str(i+1)).text = texts[i] +" "+ numerals[Global.upgrades[texts[i]]] 
-		get_node("HBoxContainer/RichTextLabel"+str(i+1)+"/AnimatedSprite2D")
+		get_node("HBoxContainer/RichTextLabel"+str(i+1)+"/AnimatedSprite2D").play(str(Global.upgrades[texts[i]]))
 
 func _on_texture_button_1_pressed() -> void:
-	pass # Replace with function body.
+	print("meow")
+	if Global.bank[Global.upgrades[texts[0]]] >= 20:
+		Global.bank[Global.upgrades[texts[0]]] -= 20
+		Global.upgrades[texts[0]] += 1
+		
 
 
 func _on_texture_button_2_pressed() -> void:
-	pass # Replace with function body.
+	if Global.bank[Global.upgrades[texts[1]]] >= 25:
+		Global.bank[Global.upgrades[texts[1]]] -= 25
+		Global.upgrades[texts[0]] += 1
+		
 
 
 func _on_texture_button_3_pressed() -> void:
-	pass # Replace with function body.
+	if Global.bank[Global.upgrades[texts[2]]] >= 30:
+		Global.bank[Global.upgrades[texts[2]]] -= 30
+		Global.upgrades[texts[0]] += 1
+		
+
+
+func _on_texture_button_4_pressed() -> void:
+	if Global.bank[Global.upgrades[texts[3]]] >= 35:
+		Global.bank[Global.upgrades[texts[3]]] -= 35
+		Global.upgrades[texts[0]] += 1
+		
+
+
+func _on_texture_button_5_pressed() -> void:
+	if Global.bank[Global.upgrades[texts[4]]] >= 45:
+		Global.bank[Global.upgrades[texts[4]]] -= 45
+		Global.upgrades[texts[0]] += 1
+		
+
+
+func _on_profanity_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
